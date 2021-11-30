1606dadd18c13e5152c56669bdd134eb90995ebe
Dominik Honzak <49328567+darkninja1@users.noreply.github.com>
2021-11-30 14:21:24 +0000

    ...

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function m2() {
+  document.write('hi');
   var text = document.getElementById('m1').value;
   if (text.includes('sqrt')) {
     text.replace('sqrt','√');
-    document.getElementById('m1').value = text;
+    
   }
+  //document.getElementById('m1').value = text;
 }
