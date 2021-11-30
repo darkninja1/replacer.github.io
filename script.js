42837fd614f9ee3f98616d0dacf6880375762ecf
Dominik Honzak <49328567+darkninja1@users.noreply.github.com>
2021-11-30 14:09:27 +0000

    ...

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function math() {
-  var text = document.getElementById('math').value;
+function math1() {
+  var text = document.getElementById('math1').value;
   if (text.includes('sqrt')) {
     text.replace('sqrt','√');
   }
