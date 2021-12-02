065ab5c20144572a512543d1af6571ff469ce737
Dominik Honzak <49328567+darkninja1@users.noreply.github.com>
2021-12-02 19:49:31 +0000

    ...

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ function m2(e) {
   //document.write('hi');
   var text1 = document.getElementById('m1').value;
   var new1 = text1;
-  if (text1.includes("sqrt")) {
+  if (new1.includes("sqrt")) {
     new1.replace("sqrt","√");
   }
-  else if (text1.includes("_")) {
+  else if (new1.includes("_")) {
     new1.replace("_","<sub>");
   }
-  else if (text1.includes("^")) {
+  else if (new1.includes("^")) {
     new1.replace("^","<sup>");
   }
   document.getElementById('m3').innerHTML = new1;
