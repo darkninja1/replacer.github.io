c0922d77d5c337b4ab9e328749a0dbbace66321b
Dominik Honzak <49328567+darkninja1@users.noreply.github.com>
2021-12-04 17:20:39 +0000

    ...

--- script.js
+++ script.js
@@ -6,15 +6,13 @@ form.addEventListener('submit', function(e) {
     
     var text1 = document.getElementById('m2').value;
 
-    //if (text1.includes("sqrt")) {
+    
     var end1 = text1.replace("sqrt","√");
-    //}
-    //else if (text1.includes("_")) {
+    
     var end2 = end1.replace("_","<sub>");
-    //}
-    //else if (text1.includes("^")) {
+    
     var end3 = end2.replace("^","<sup>");
-    //}
+    
     document.getElementById('m3').innerHTML = end3;
     
 });
