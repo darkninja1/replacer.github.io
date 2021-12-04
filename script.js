ac113c13515c3275790a21395f982792ac838753
Dominik Honzak <49328567+darkninja1@users.noreply.github.com>
2021-12-04 17:02:56 +0000

    ...

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-function m2(e) {
-  e.preventDefault();
-  //document.write('hi');
-  var text1 = document.getElementById('m1').value;
-  var new1 = text1;
-  if (new1.includes("sqrt")) {
-    new1.replace("sqrt","√");
-  }
-  else if (new1.includes("_")) {
-    new1.replace("_","<sub>");
-  }
-  else if (new1.includes("^")) {
-    new1.replace("^","<sup>");
-  }
-  document.getElementById('m3').innerHTML = new1;
-}
+//
+var form = document.getElementById("m1");
+//
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    var text1 = document.getElementById('m2').value;
+
+    if (text1.includes("sqrt")) {
+      text1.replace("sqrt","√");
+    }
+    else if (text1.includes("_")) {
+      text1.replace("_","<sub>");
+    }
+    else if (text1.includes("^")) {
+      text1.replace("^","<sup>");
+    }
+    else {
+      document.write("Invalid Url or Error");
+    }
+    document.getElementById('m3').innerHTML = text1;
+    
+});
+  
+  
+
+
